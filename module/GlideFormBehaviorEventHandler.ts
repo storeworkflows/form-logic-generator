71cfc4d29be402da453f1a3fee18cfd811c70775
artur-bunko <artur.bunko@storeworkflows.com>
2021-04-19 14:27:35 +0300

    feat: Event prop update

--- module/GlideFormBehaviorEventHandler.ts
+++ module/GlideFormBehaviorEventHandler.ts
@@ -141,6 +141,7 @@ export default class GlideFormBehaviorEventHandler {
                             if (section.caption === name) {
                                 section[propName] = value;
                             }
+                            return section
                         })
                     }
                 })
